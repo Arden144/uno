714933f71a982ced1dc0fd2a677b8665bea37494
Arden <general@ardensinclair.com>
2023-07-05 00:41:35 -0700

    Rewrote some functions for clarity

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@
 
 module Main (main) where
 
+import Control.Monad.State.Lazy (MonadState (get, put), State, evalState)
 import Data.List (find, sortBy)
 import Data.List.Extra (delete, firstJust)
 import Data.Map qualified as Map
@@ -15,7 +16,7 @@ import Raylib.Types (MouseButton (MouseButtonLeft), Rectangle (Rectangle, rectan
 import Raylib.Types qualified (Color)
 import Raylib.Util (whileWindowOpen_)
 import Raylib.Util.Colors (black, blue, green, rayWhite, red, white, yellow)
-import System.Random (initStdGen)
+import System.Random (RandomGen, initStdGen)
 import System.Random.Shuffle (shuffle')
 
 data Color = Red | Yellow | Green | Blue deriving (Enum, Eq, Ord)
@@ -97,8 +98,8 @@ pileCard :: Rectangle
 pileCard = Rectangle (fromIntegral width / 2 - cardWidth / 2) (fromIntegral height / 2 - cardHeight / 2) cardWidth cardHeight
 
 -- Draws the background of an uno card
-drawCardBackground :: Float -> Float -> Raylib.Types.Color -> IO ()
-drawCardBackground x y c =
+drawCardBackground :: Raylib.Types.Color -> Float -> Float -> IO ()
+drawCardBackground c x y =
   do
     drawRectangleRounded (Rectangle x y cardWidth cardHeight) 0.2 0 white
     drawRectangleRounded (Rectangle (x + cardBorder) (y + cardBorder) (cardWidth - 2 * cardBorder) (cardHeight - 2 * cardBorder)) 0.2 0 c
@@ -115,20 +116,20 @@ drawWildcardBackground x y =
 instance Draw Card where
   draw (Normal c n, x, y) =
     do
-      drawCardBackground x y (getRlColor c)
+      drawCardBackground (getRlColor c) x y
       drawText (show n) (round (x + 4 * cardBorder)) (round (y + 4 * cardBorder)) 36 black
   draw (Skip c, x, y) =
     do
-      drawCardBackground x y (getRlColor c)
+      drawCardBackground (getRlColor c) x y
       drawRing (Vector2 (x + 5 * cardBorder) (y + 5 * cardBorder)) 10 13 0 360 0 black
       drawLineEx (Vector2 (x + 5 * cardBorder - 12) (y + 5 * cardBorder)) (Vector2 (x + 5 * cardBorder + 12) (y + 5 * cardBorder)) 3 black
   draw (Reverse c, x, y) =
     do
-      drawCardBackground x y (getRlColor c)
+      drawCardBackground (getRlColor c) x y
       drawText "<=>" (round (x + 4 * cardBorder)) (round (y + 4 * cardBorder)) 36 black
   draw (Draw c, x, y) =
     do
-      drawCardBackground x y (getRlColor c)
+      drawCardBackground (getRlColor c) x y
       drawText "+2" (round (x + 4 * cardBorder)) (round (y + 4 * cardBorder)) 36 black
   draw (Wild Nothing, x, y) =
     do
@@ -136,7 +137,7 @@ instance Draw Card where
       drawText "wild" (round (x + 4 * cardBorder)) (round (y + 4 * cardBorder)) 36 black
   draw (Wild (Just c), x, y) =
     do
-      drawCardBackground x y (getRlColor c)
+      drawCardBackground (getRlColor c) x y
       drawText "wild" (round (x + 4 * cardBorder)) (round (y + 4 * cardBorder)) 36 black
   draw (WildDraw Nothing, x, y) =
     do
@@ -144,41 +145,63 @@ instance Draw Card where
       drawText "+4" (round (x + 4 * cardBorder)) (round (y + 4 * cardBorder)) 36 black
   draw (WildDraw (Just c), x, y) =
     do
-      drawCardBackground x y (getRlColor c)
+      drawCardBackground (getRlColor c) x y
       drawText "+4" (round (x + 4 * cardBorder)) (round (y + 4 * cardBorder)) 36 black
 
 -- Draws the back of a card
 drawBlankCard :: Float -> Float -> IO ()
-drawBlankCard x y = drawCardBackground x y black
+drawBlankCard = drawCardBackground black
+
+listReplicate :: Int -> [a] -> [a]
+listReplicate = concatMap . replicate
 
 -- Creates all of the preset cards in a deck
 allCards :: Deck
 allCards =
-  ((\c -> Normal c Zero : concat (replicate 2 ([Skip c, Reverse c, Draw c] ++ (Normal c <$> enumFrom One)))) =<< enumFrom Red)
+  ([Red ..] >>= colorCards)
     ++ replicate 4 (Wild Nothing)
     ++ replicate 4 (WildDraw Nothing)
+  where
+    colorCards c =
+      zero : listReplicate 2 special ++ normal
+      where
+        zero = Normal c Zero
+        special = [Skip c, Reverse c, Draw c]
+        normal = Normal c <$> [One ..]
 
 -- Deals 7 cards
-deal :: Deck -> (Deck, Deck)
-deal = splitAt 7
+deal :: State Deck Deck
+deal =
+  get >>= \pile ->
+    let (dealt, newPile) = splitAt 7 pile
+     in put newPile >> pure dealt
 
 -- Checks if a card is a valid starter card for the pile
 isStarterCard :: Card -> Bool
 isStarterCard (Normal _ _) = True
 isStarterCard _ = False
 
--- Creates the initial state by shuffling the cards and drawing for the players
-genInitialState :: IO Model
-genInitialState =
-  do
-    gen <- initStdGen
-    let pile = shuffle' allCards (length allCards) gen
+getTopCard :: State Deck Card
+getTopCard =
+  get >>= \pile ->
     let top = fromMaybe (head pile) (find isStarterCard pile)
-    let pile2 = delete top pile
-    let (player, pile3) = deal pile2
-    let (cpu, pile4) = deal pile3
+     in put (delete top pile) >> pure top
 
-    return Model {top, player, cpu, pile = pile4}
+-- Creates the initial state by shuffling the cards and drawing for the players
+mkState :: (RandomGen gen) => gen -> Model
+mkState gen =
+  evalState setup deck
+  where
+    deck = shuffle' allCards (length allCards) gen
+    setup = do
+      top <- getTopCard
+      player <- deal
+      cpu <- deal
+      pile <- get
+      return Model {top, player, cpu, pile}
+
+genInitialState :: IO Model
+genInitialState = mkState <$> initStdGen
 
 -- Checks if a card in the player's deck was clicked and issues an update
 deckCardUpdate :: Vector2 -> [(Card, Float, Float)] -> Update
@@ -237,7 +260,7 @@ countColors (card : cards) = case getColor card of
 
 -- Finds all valid moves in a deck given the top card
 allMoves :: Card -> Deck -> Deck
-allMoves top = filter (isValidMove top)
+allMoves = filter . isValidMove
 
 -- Finds the best move with a card of the given color
 colorMove :: Deck -> Color -> Maybe Card
@@ -245,7 +268,7 @@ colorMove [] _ = Nothing
 colorMove ((Wild _) : xs) c = colorMove xs c
 colorMove ((WildDraw _) : xs) c = colorMove xs c
 colorMove (x : xs) c
-  | all (== c) (getColor x) = Just x
+  | c `elem` getColor x = Just x
   | otherwise = colorMove xs c
 
 -- Finds the best move given a descending list of colors ordered by count
@@ -282,10 +305,10 @@ computerDrawCard s@Model {pile = []} = s
 
 -- Gets the computer's next move if possible
 computerGetMove :: Card -> Deck -> Maybe Card
-computerGetMove top deck = listToMaybe $ catMaybes [priorityMove moves idealColor, wildMove moves idealColor]
+computerGetMove top deck = firstJust (\f -> f moves idealColor) [priorityMove, wildMove]
   where
     moves = allMoves top deck
-    idealColor = map fst $ sortBy (\(_, a) (_, b) -> compare b a) (Map.toList (countColors deck))
+    idealColor = fst <$> sortBy (\(_, a) (_, b) -> compare b a) (Map.toList (countColors deck))
 
 -- Either plays the best move or picks up if no valid move exists
 computerPlay :: Model -> Model
@@ -314,29 +337,27 @@ computerPlayCard s card@(Reverse _) =
         cpu = delete card s.cpu
       }
 computerPlayCard s card@(Draw _) =
-  s
-    { top = card,
-      player = drawn ++ s.player,
-      cpu = delete card s.cpu,
-      pile = newPile
-    }
-  where
-    (drawn, newPile) = splitAt 2 s.pile
+  let (drawn, newPile) = splitAt 2 s.pile
+   in Model
+        { top = card,
+          player = drawn ++ s.player,
+          cpu = delete card s.cpu,
+          pile = newPile
+        }
 computerPlayCard s card@(Wild (Just _)) =
   s
     { top = card,
       cpu = delete (Wild Nothing) s.cpu
     }
 computerPlayCard s card@(WildDraw (Just _)) =
-  computerPlay
-    s
-      { top = card,
-        player = drawn ++ s.player,
-        cpu = delete (WildDraw Nothing) s.cpu,
-        pile = newPile
-      }
-  where
-    (drawn, newPile) = splitAt 4 s.pile
+  let (drawn, newPile) = splitAt 4 s.pile
+   in computerPlay
+        Model
+          { top = card,
+            player = drawn ++ s.player,
+            cpu = delete (WildDraw Nothing) s.cpu,
+            pile = newPile
+          }
 computerPlayCard _ (Wild Nothing) =
   error "computer played wild with no color"
 computerPlayCard _ (WildDraw Nothing) =
@@ -362,15 +383,14 @@ playCard s card@(Reverse _) =
       player = delete card s.player
     }
 playCard s card@(Draw _) =
-  computerPlay
-    s
-      { top = card,
-        player = delete card s.player,
-        cpu = drawn ++ s.cpu,
-        pile = newPile
-      }
-  where
-    (drawn, newPile) = splitAt 2 s.pile
+  let (drawn, newPile) = splitAt 2 s.pile
+   in computerPlay
+        Model
+          { top = card,
+            player = delete card s.player,
+            cpu = drawn ++ s.cpu,
+            pile = newPile
+          }
 playCard s card@(Wild (Just _)) =
   computerPlay
     s
@@ -378,14 +398,13 @@ playCard s card@(Wild (Just _)) =
         player = delete (Wild Nothing) s.player
       }
 playCard s card@(WildDraw (Just _)) =
-  s
-    { top = card,
-      player = delete (WildDraw Nothing) s.player,
-      cpu = drawn ++ s.cpu,
-      pile = newPile
-    }
-  where
-    (drawn, newPile) = splitAt 4 s.pile
+  let (drawn, newPile) = splitAt 4 s.pile
+   in Model
+        { top = card,
+          player = delete (WildDraw Nothing) s.player,
+          cpu = drawn ++ s.cpu,
+          pile = newPile
+        }
 playCard _ (Wild Nothing) =
   error "player played wild with no color"
 playCard _ (WildDraw Nothing) =
@@ -453,6 +472,7 @@ mainLoop s@Model {pile = []} = do
   return s
 mainLoop s = do
   let playerCardLocations = (\(card, x) -> (card, x, playerCardY)) <$> zip s.player [cardSpacing, (cardSpacing * 2) ..]
+
   beginDrawing
 
   clearBackground rayWhite
@@ -481,5 +501,6 @@ main :: IO ()
 main = do
   window <- initWindow width height "Uno"
   setTargetFPS 60
-  genInitialState >>= whileWindowOpen_ mainLoop
+  s <- genInitialState
+  whileWindowOpen_ mainLoop s
   closeWindow window
